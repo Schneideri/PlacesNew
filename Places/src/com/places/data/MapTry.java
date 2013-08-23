5c46de7c9ebe62f541e9f875ef3aea291c3d0c89
Tania <drob.tania@gmail.com>
2013-08-23 14:05:54 +0300

    maptry removed comments

--- Places/src/com/places/data/MapTry.java
+++ Places/src/com/places/data/MapTry.java
@@ -16,25 +16,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 
 public class MapTry extends FragmentActivity
 {
-		/*private GoogleMap map;
-		//private MainFragment fragment;
-
-		@Override
-		public void onCreate(Bundle bundle) 
-		{
-		    super.onCreate(bundle);
-		    
-		    setContentView(R.layout.maptry);
-		    
-	        if (GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext()) == 0)
-	        {
-			    map = ((MapFragment) getFragmentManager().findFragmentById(R.id.mvMapView)).getMap();
-			    map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-	        }
-		}*/
-	
-	//getSupportFragmentManager().findFragmentById(R.id.mvMapView))
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
@@ -45,21 +26,10 @@ public class MapTry extends FragmentActivity
 
 	    
 	    FragmentManager myFM = getSupportFragmentManager();  
-	       SupportMapFragment myMAPF = (SupportMapFragment) myFM  
+	    SupportMapFragment myMAPF = (SupportMapFragment) myFM  
 	                 .findFragmentById(R.id.mvMapView);
 
-	       GoogleMap map = myMAPF.getMap();
-
-
-	    //SupportMapFragment fragment = new SupportMapFragment();
-	   /* getSupportFragmentManager().beginTransaction()
-	            .add(android.R.id.content, fragment).commit();
-
-	    setContentView(R.layout.activity_main);*/
-
-	    //GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
-
-	    //GoogleMap map = ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.mvMapView)).getMap();
+	    GoogleMap map = myMAPF.getMap();
 	}
 
 }
